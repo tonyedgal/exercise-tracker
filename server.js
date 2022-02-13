6cfb8d5480856f4bd8c21133b8af553592524ba2
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-02-13 05:05:03 +0100

    Feat: Added .env variables
    - stored variables in .env file
    - created database connection in server.js
    - deleted sample.env file

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 
+const port = process.env.PORT || 8000;
+const dbURI = process.env.MONGODB_URI;
+
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to Database"))
+  .catch((error) => console.log(error));
 
 const listener = app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
