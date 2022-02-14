d5d100a1bcefe79ad25d75537f84b549b1af70ca
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-02-14 11:08:07 +0100

    Feat: Created user and session schema in server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
+const { Schema } = mongoose;
 
 const port = process.env.PORT || 8000;
 const dbURI = process.env.MONGODB_URI;
@@ -14,6 +15,20 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+const exerciseSessionSchema = new Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: String,
+});
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  log: [exerciseSessionSchema],
+});
+
+const User = mongoose.model("User", userSchema);
+const Session = mongoose.model("Session", exerciseSessionSchema);
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
